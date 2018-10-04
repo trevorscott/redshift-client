5ed9669c7ca70514705eaa3f8610a12c9c6519ed
Trevor Scott <tscott@heroku.com>
2018-10-04 12:33:14 -0700

    fix test query

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const redshiftClient = require('./redshift.js');
 
-redshiftClient.connect(function(err){
-  if(err) throw err;
-  else{
-    redshiftClient.query('SELECT * FROM "TableName"', function(err, data){
-      if(err) throw err;
-      else{
-        console.log(data);
-        redshiftClient.close();
-      }
-    });
-  }
+// options is an optional object with one property so far {raw: true} returns 
+// just the data from redshift. {raw: false} returns the data with the pg object
+const queryString = "select count(*) from systables";
+
+redshiftClient.query(queryString, {raw: false})
+.then(function(data){
+    console.log(data);
+})
+.catch(function(err){
+    console.error(err);
 });
