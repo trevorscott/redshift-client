bfe08325d585005c9f01f98f2348840b549762d4
Trevor Scott <tscott@heroku.com>
2018-10-11 13:16:43 -0700

    debug

--- index.js
+++ index.js
@@ -9,7 +9,15 @@ const { Pool } = require('pg')
 
 const PORT = process.env.PORT || 5000;
 
-const pool = new Pool({
+const redshiftPool = new Pool({
+  user: process.env.REDSHIFT_USERNAME,
+  host: process.env.REDSHIFT_HOST,
+  database: process.env.REDSHIFT_DATABASE,
+  password: process.env.REDSHIFT_PASSWORD,
+  port: 5439
+});
+
+const postgresPool = new Pool({
   user: process.env.REDSHIFT_USERNAME,
   host: process.env.REDSHIFT_HOST,
   database: process.env.REDSHIFT_DATABASE,
@@ -34,11 +42,11 @@ if (cluster.isMaster) {
 } else {
   const app = express();
 
-  const text = "SELECT * FROM pg_catalog.pg_tables;";
-
-  pool.query(text)
+  const redshiftTestQuery = "SELECT * FROM pg_catalog.pg_tables;";
+  redshiftPool.query(redshiftTestQuery)
     .then(r => {
-      console.log(`Successfully connected to AWS Redshift Cluster: ${process.env.REDSHIFT_HOST}:${process.env.REDSHIFT_DATABASE}.`)
+      console.log(r);
+      console.log(`Successfully connected to AWS Redshift Cluster: ${process.env.REDSHIFT_HOST}:${process.env.REDSHIFT_DATABASE}`)
     })
     .catch(e =>{         
       throw e;
